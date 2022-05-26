0bf663e85797e470bdf6c92a7f94e0c1b9ad5d23
thomas914 <thomasli914@gmail.com>
2022-05-26 10:42:01 +0800

    Briefly looked into fetching HK stock issue

--- application/grap_stock_data_application.py
+++ application/grap_stock_data_application.py
@@ -1,8 +1,10 @@
 import sys
 import pathlib
+
 sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.resolve()))
 
 from engine.realtime_engine_ibkr.stock_data_engine import *
+
 
 def main():
     # if you want to change the output path, go to __init__ of ibkr_stock_data_io_engine to change it
@@ -10,8 +12,9 @@ def main():
     ib = IB()
     ib.connect('127.0.0.1', 7497, clientId=1)
     engine = ibkr_stock_data_io_engine(ib)
-    engine.get_historical_data_by_range("AAPL",1609430400,1643594952,"1 min",False)
+    engine.get_historical_data_by_range("AAPL", 1609430400, 1643594952, "1 min", False)
     # if the process is interrupted in the middle, keep the start timestamp and change the end timestamp in the "get_historical_data_by_range" to the earliest timestamp in the csv file -> run the program again
+
 
 if __name__ == "__main__":
     main()
--- engine/realtime_engine_ibkr/stock_data_engine.py
+++ engine/realtime_engine_ibkr/stock_data_engine.py
@@ -6,11 +6,13 @@ import pandas as pd
 
 import sys
 import pathlib
+
 sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.parent.resolve()))
 from failure_handler import connection_handler, connect_tws
 
 """note: ALL the returning datetime will be set according to the BASE (but WITHOUT timezone info), i.e., Hong Kong time in this case. Timestamp should be adjusted accordingly"""
 """all the time input and comparison must be done according to the BASE timezone, i.e., Hong Kong time"""
+
 
 # # helper functions
 # def timestamp_to_hk_dt_string(timestamp): # useless now
@@ -29,12 +31,13 @@ def raw_data_to_df(raw_data):
     print(df)
     return df
 
-def combine_csv(filepath,csv_list,output_name):
+
+def combine_csv(filepath, csv_list, output_name):
     df_list = []
     for csv in csv_list:
         df = pd.read_csv(f"{filepath}/{csv}")
         df_list.append(df)
-        
+
     full_df = pd.concat(df_list)
 
     # remove the output file if exist
@@ -46,26 +49,28 @@ def combine_csv(filepath,csv_list,output_name):
             print(f"Some errors occur, error message: {e}")
 
     # write the entire df to the output file
-    with open(f"{filepath}/{output_name}.csv","a+",newline='') as f:
-        full_df.to_csv(f,mode='a',index=False,header=True) # write the current data with header
+    with open(f"{filepath}/{output_name}.csv", "a+", newline='') as f:
+        full_df.to_csv(f, mode='a', index=False, header=True)  # write the current data with header
 
     print("Successfully combined csv files")
+
 
 class ibkr_stock_data_io_engine:
     ib_instance = None
     output_filepath = ""
 
-    def __init__(self,ib_instance):
+    def __init__(self, ib_instance):
         self.ib_instance = ib_instance
-        self.ib_instance.reqMarketDataType(marketDataType=1) # require live data
+        self.ib_instance.reqMarketDataType(marketDataType=1)  # require live data
         # self.output_filepath = str(pathlib.Path(__file__).parent.parent.parent.resolve()) + f"/his_data/one_min"
-        self.output_filepath = str(pathlib.Path(__file__).parent.parent.parent.parent.parent.resolve()) + "/ticker_data/one_min"
+        self.output_filepath = str(
+            pathlib.Path(__file__).parent.parent.parent.parent.resolve()) + "/ticker_data/one_min"
         # self.output_filepath = "C:/Users/85266/OneDrive/Documents"
 
     # return a dictionary of the latest price of the stock
-    
+
     @connection_handler
-    def get_ibkr_open_price(self,tickers):
+    def get_ibkr_open_price(self, tickers):
         """
         Example:
         input:
@@ -73,52 +78,62 @@ class ibkr_stock_data_io_engine:
         """
         connect_tws(self.ib_instance)
 
-        ticker_info_dict = {} # the dictionary to be returned
-        contracts = {} # a dictionary of the contract object
+        ticker_info_dict = {}  # the dictionary to be returned
+        contracts = {}  # a dictionary of the contract object
         for ticker in tickers:
-            contracts[ticker] = Stock(ticker,"SMART","USD") # create the contract in the dictionary
-            self.ib_instance.qualifyContracts(contracts[ticker]) # qualify the contract
-            self.ib_instance.reqMktData(contracts[ticker],'',False,False) # subsribe the market data of the contact
-            self.ib_instance.sleep(0) # update the ib instance
+            contracts[ticker] = Stock(ticker, "SMART", "USD")  # create the contract in the dictionary
+            self.ib_instance.qualifyContracts(contracts[ticker])  # qualify the contract
+            self.ib_instance.reqMktData(contracts[ticker], '', False, False)  # subsribe the market data of the contact
+            self.ib_instance.sleep(0)  # update the ib instance
             raw_ticker_info = self.ib_instance.reqTickers(contracts[ticker])[0]
-            self.ib_instance.sleep(1) # allows the Tickers info to fill gradually
+            self.ib_instance.sleep(1)  # allows the Tickers info to fill gradually
 
             # fetch until information is completed
-            while math.isnan(raw_ticker_info.bid) or math.isnan(raw_ticker_info.bidSize) or math.isnan(raw_ticker_info.ask) or math.isnan(raw_ticker_info.askSize) or math.isnan(raw_ticker_info.last) or math.isnan(raw_ticker_info.lastSize) or math.isnan(raw_ticker_info.volume):
-                print("Information incompleted, data:",raw_ticker_info)
+            while math.isnan(raw_ticker_info.bid) or math.isnan(raw_ticker_info.bidSize) or math.isnan(
+                    raw_ticker_info.ask) or math.isnan(raw_ticker_info.askSize) or math.isnan(
+                    raw_ticker_info.last) or math.isnan(raw_ticker_info.lastSize) or math.isnan(raw_ticker_info.volume):
+                print("Information incompleted, data:", raw_ticker_info)
                 print("Trying again...")
-                contracts[ticker] = Stock(ticker,"SMART","USD") # create the contract in the dictionary
-                self.ib_instance.qualifyContracts(contracts[ticker]) # qualify the contract
-                self.ib_instance.reqMktData(contracts[ticker],'',False,False) # subsribe the market data of the contact
-                self.ib_instance.sleep(0) # update the ib instance
+                contracts[ticker] = Stock(ticker, "SMART", "USD")  # create the contract in the dictionary
+                self.ib_instance.qualifyContracts(contracts[ticker])  # qualify the contract
+                self.ib_instance.reqMktData(contracts[ticker], '', False,
+                                            False)  # subsribe the market data of the contact
+                self.ib_instance.sleep(0)  # update the ib instance
                 raw_ticker_info = self.ib_instance.reqTickers(contracts[ticker])[0]
-                self.ib_instance.sleep(1) # allows the Tickers info to fill gradually
+                self.ib_instance.sleep(1)  # allows the Tickers info to fill gradually
 
             print(raw_ticker_info)
-            
-            ticker_info = {"timestamp":raw_ticker_info.time.timestamp(),"bid":raw_ticker_info.bid,"bidSize":raw_ticker_info.bidSize,"ask":raw_ticker_info.ask,"askSize":raw_ticker_info.askSize,"last":raw_ticker_info.last,"lastSize":raw_ticker_info.lastSize,"volume":raw_ticker_info.volume,"close":raw_ticker_info.close,"halted":raw_ticker_info.halted}
+
+            ticker_info = {"timestamp": raw_ticker_info.time.timestamp(), "bid": raw_ticker_info.bid,
+                           "bidSize": raw_ticker_info.bidSize, "ask": raw_ticker_info.ask,
+                           "askSize": raw_ticker_info.askSize, "last": raw_ticker_info.last,
+                           "lastSize": raw_ticker_info.lastSize, "volume": raw_ticker_info.volume,
+                           "close": raw_ticker_info.close, "halted": raw_ticker_info.halted}
             ticker_info_dict[ticker] = ticker_info
-            
+
         return ticker_info_dict
 
     # get data by passing tin the start timestamp and the end timestamp
     # there may be request limit for this function, while the limit if set by TWS
     """just the helper function, NOT called directly"""
-    def get_historical_data_helper(self,ticker,end_timestamp,duration,bar_size,regular_trading_hour): 
+
+    def get_historical_data_helper(self, ticker, end_timestamp, duration, bar_size, regular_trading_hour):
         """
         end_timestamp: an unix timestamp
         duration: the length of data to be retrieved (couting bask starting fron the end_date), Examples: ‘60 S’, ‘30 D’, ‘13 W’, ‘6 M’, ‘10 Y’
         bar_size: the timelapse between 2 data, must be one of: ‘1 secs’, ‘5 secs’, ‘10 secs’ 15 secs’, ‘30 secs’, ‘1 min’, ‘2 mins’, ‘3 mins’, ‘5 mins’, ‘10 mins’, ‘15 mins’, ‘20 mins’, ‘30 mins’, ‘1 hour’, ‘2 hours’, ‘3 hours’, ‘4 hours’, ‘8 hours’, ‘1 day’, ‘1 week’, ‘1 month’.
         regular_trading_hour: a switch (boolean value) to allow user to choose whether to get data in the regular trading hour (True) or not (False)
         """
-        end_date = dt.datetime.fromtimestamp(end_timestamp,tz=dt.timezone(dt.timedelta(hours=8)))
-        contract = Stock(ticker,"SMART","USD") # create the contract in the dictionary
-        self.ib_instance.qualifyContracts(contract) # qualify the contract
-        data = self.ib_instance.reqHistoricalData(contract,end_date,durationStr=duration,barSizeSetting=bar_size,whatToShow="TRADES",useRTH=regular_trading_hour)
-        self.ib_instance.sleep(1) # allows the data to fill gradually
+        end_date = dt.datetime.fromtimestamp(end_timestamp, tz=dt.timezone(dt.timedelta(hours=8)))
+        # contract = Stock(ticker, "SMART", "USD")  # create the contract in the dictionary
+        contract = Stock('3188', 'SEHK', 'HKD')  # need to check ib permission, by Thomas Li
+        self.ib_instance.qualifyContracts(contract)  # qualify the contract
+        data = self.ib_instance.reqHistoricalData(contract, end_date, durationStr=duration, barSizeSetting=bar_size,
+                                                  whatToShow="TRADES", useRTH=regular_trading_hour)
+        self.ib_instance.sleep(1)  # allows the data to fill gradually
 
         return data
-    
+
     """
     Details of the head timestamp for each stocks:
     QQQ: 1999-03-10 14:30:00
@@ -129,35 +144,37 @@ class ibkr_stock_data_io_engine:
     UPRO: 2009-06-26 08:00:00
     // 2009-07-01 00:00:00 - 1246377600
     """
-    # write the historical data to the 
+
+    # write the historical data to the
     # the function is for fetching large dataframe, thus inside the loop will only fetch the data of one week once
     # due to TWS limitataion, max. 2 tickers at a time !!!
     # e.g. {"QQQ":[{timestamp, ohlc},{timestamp, ohlc}],"SPY"[{timestamp, ohlc},{timestamp, ohlc}]...}
-    def get_historical_data_by_range(self,ticker,start_timestamp,end_timestamp,bar_size,regular_trading_hour):
-        
+    def get_historical_data_by_range(self, ticker, start_timestamp, end_timestamp, bar_size, regular_trading_hour):
+
         # historical_data = []
         current_end_timestamp = end_timestamp
 
         while (current_end_timestamp > start_timestamp):
-            current_data = self.get_historical_data_helper(ticker,current_end_timestamp,"3 W",bar_size,regular_trading_hour)
+            current_data = self.get_historical_data_helper(ticker, current_end_timestamp, "3 W", bar_size,
+                                                           regular_trading_hour)
             front_timestamp = current_data[0].date.replace(tzinfo=dt.timezone(dt.timedelta(hours=8))).timestamp()
             # historical_data = current_data + historical_data # put the new data in front
             print(f"Fetched three weeks data for {ticker}, from {int(front_timestamp)} to {int(current_end_timestamp)}")
             current_end_timestamp = front_timestamp
             # sleep(10) # wait to fetch another batch of data
-            self.ib_instance.sleep(0) # refresh the ib instance
-            current_data_df = util.df(current_data) # convert into df
+            self.ib_instance.sleep(0)  # refresh the ib instance
+            current_data_df = util.df(current_data)  # convert into df
             # adding a column of timestamp
-            current_data_df['timestamp'] = current_data_df[['date']].apply(lambda x: x[0].replace(tzinfo=dt.timezone(dt.timedelta(hours=8))).timestamp(), axis=1).astype(int)
+            current_data_df['timestamp'] = current_data_df[['date']].apply(
+                lambda x: x[0].replace(tzinfo=dt.timezone(dt.timedelta(hours=8))).timestamp(), axis=1).astype(int)
             # write to csv
-            self.write_df_to_csv(ticker,current_data_df)
-        
+            self.write_df_to_csv(ticker, current_data_df)
+
         # adding a column of timestamp
         # historical_data['timestamp'] = historical_data[['date']].apply(lambda x: x[0].replace(tzinfo=dt.timezone(dt.timedelta(hours=8))).timestamp(), axis=1).astype(int)
         # list(map(lambda x: {"date":x.date,"timestamp":x.date.replace(tzinfo=dt.timezone(dt.timedelta(hours=8))).timestamp(),"open":x.open,"high":x.high,"low":x.low,"close":x.close,"volume":x.volume,"average":x.average,"barCount":x.barCount},historical_data[ticker]))
 
-    
-    def write_df_to_csv(self,ticker,df):
+    def write_df_to_csv(self, ticker, df):
         """
         algoithm:
         if file already exists:
@@ -168,31 +185,33 @@ class ibkr_stock_data_io_engine:
         write the old data if file already exist
         """
         file_exist = f"{ticker}.csv" in os.listdir(self.output_filepath)
-        if file_exist: # file already exist
+        if file_exist:  # file already exist
             old_df = pd.read_csv(f"{self.output_filepath}/{ticker}.csv")
             try:
                 os.remove(f"{self.output_filepath}/{ticker}.csv")
             except Exception as e:
                 print(f"Some errors occur, error message: {e}")
-        
-        with open(f"{self.output_filepath}/{ticker}.csv","a+",newline='') as f:
-            df.to_csv(f,mode='a',index=False,header=True) # write the current data with header
+
+        with open(f"{self.output_filepath}/{ticker}.csv", "a+", newline='') as f:
+            df.to_csv(f, mode='a', index=False, header=True)  # write the current data with header
             if file_exist:
-                old_df.to_csv(f,mode='a',index=False,header=False) # write the old data
+                old_df.to_csv(f, mode='a', index=False, header=False)  # write the old data
 
         print(f"Successfully appended {ticker}.csv")
 
-    def get_multiple_historical_data_by_range(self,tickers,start_timestamp,end_timestamp,bar_size,regular_trading_hour):
+    def get_multiple_historical_data_by_range(self, tickers, start_timestamp, end_timestamp, bar_size,
+                                              regular_trading_hour):
         for ticker in tickers:
-            self.get_historical_data_by_range(ticker,start_timestamp,end_timestamp,bar_size,regular_trading_hour)
+            self.get_historical_data_by_range(ticker, start_timestamp, end_timestamp, bar_size, regular_trading_hour)
             print("successfully written", ticker)
+
 
 def main():
     ib = IB()
     ib.connect('127.0.0.1', 7497, clientId=1)
     # contracts = [Stock(ticker,"SMART","USD") for ticker in tickers]
     engine = ibkr_stock_data_io_engine(ib)
-    engine.get_ibkr_open_price(["QQQ","SPY"])
+    engine.get_ibkr_open_price(["QQQ", "SPY"])
     # print(engine.get_ibkr_open_price("QQQ"))
     # for contract in contracts:
     #     ib.qualifyContracts(contract)
@@ -230,6 +249,7 @@ def main():
     # ticker = ib.reqTickers(contracts[0])
     # ib.sleep(1)
     # print(ticker[0].time)
+
 
 if __name__ == "__main__":
     main()
