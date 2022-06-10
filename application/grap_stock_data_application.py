47ed630c7000e2af027c0b0e3355aa21f79e1dd6
Percy124 <percychui11@gmail.com>
2022-06-10 16:49:15 +0800

    .

--- application/grap_stock_data_application.py
+++ application/grap_stock_data_application.py
@@ -1,4 +1,5 @@
-import requests
+from datetime import datetime
+from datetime import timezone
 
 from engine.realtime_engine_ibkr.stock_data_engine import *
 from engine.backtest_engine.currencies_engine import *
@@ -19,10 +20,9 @@ def main():
     # to the earliest timestamp in the csv file -> run the program again
 
     # The following two lines are fetching stock/forex data
-    engine.get_historical_data_by_range("QQQ",
-                                        dt.datetime(year=2004, month=3, day=10, tzinfo=dt.timezone.utc).timestamp(),
-                                        dt.datetime(year=2004, month=7, day=1, tzinfo=dt.timezone.utc).timestamp(),
-                                        "1 min", False)
+    engine.get_historical_data_by_range("QQQ", 0, datetime.timestamp(
+        datetime(year=2022, month=6, day=1, tzinfo=timezone.utc)), "1 min", False)
+
     # engine.get_historical_currency_rate_by_range('USD', 'HKD', 946702800, 1590700500)
     # engine.get_sehk_historical_data_by_range('3188', 1351215000, 1351216020, '1 min', True)
 
