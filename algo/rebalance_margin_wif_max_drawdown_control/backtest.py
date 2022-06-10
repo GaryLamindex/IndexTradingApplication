88a4d5ec2cdd7ee6c8794fd692f7e0b0b48757d7
jwshc <jwshc@connect.hku.hk>
2022-06-10 10:36:42 +0800

    average win per day

--- algo/rebalance_margin_wif_max_drawdown_control/backtest.py
+++ algo/rebalance_margin_wif_max_drawdown_control/backtest.py
@@ -258,6 +258,13 @@ class backtest(object):
 
                 drawdown_dict = stat_engine.get_drawdown_data(file_name, date_range)
 
+                average_win_day_dict = stat_engine_3.get_average_win_day_data(file_name)
+                inception_average_win_day = average_win_day_dict.get('inception')
+                _1_yr_average_win_day = average_win_day_dict.get('1y')
+                _3_yr_average_win_day = average_win_day_dict.get('3y')
+                _5_yr_average_win_day = average_win_day_dict.get('5y')
+                _ytd_average_win_day = average_win_day_dict.get('ytd')
+
                 all_file_stats_row = {
                     "Backtest Spec": file_name, 'YTD Return': _ytd_return, '1 Yr Return': _1_yr_return,
                     "3 Yr Return": _3_yr_return, "5 Yr Return": _5_yr_return,
@@ -283,7 +290,12 @@ class backtest(object):
                     "3 Yr Rolling Return": _3_yr_rolling_return, "5 Yr Rolling Return": _5_yr_rolling_return,
                     "7 Yr Rolling Return": _7_yr_rolling_return, "10 Yr Rolling Return": _10_yr_rolling_return,
                     "15 Yr Rolling Return": _15_yr_rolling_return, "20 Yr Rolling Return": _20_yr_rolling_return,
-                    "Drawdowns": drawdown_dict
+                    "Drawdowns": drawdown_dict,
+
+                    "Since Inception Average Win Per Day": inception_average_win_day,
+                    "YTD Average Win Per Day": _ytd_average_win_day, "1 Yr Average Win Per Day": _1_yr_average_win_day,
+                    "3 Yr Average Win Per Day": _3_yr_average_win_day, "5 Yr Average Win Per Day": _5_yr_average_win_day
+
                 }
                 # _additional_data = self.cal_additional_data(file_name)
                 # data_list.append(all_file_stats_row | _additional_data)
@@ -301,7 +313,10 @@ class backtest(object):
                "Since Inception Win Rate", "YTD Win Rate", "1 Yr Win Rate", "3 Yr Win Rate", "5 Yr Win Rate",
                "1 Yr Rolling Return", "2 Yr Rolling Return", "3 Yr Rolling Return", "5 Yr Rolling Return",
                "7 Yr Rolling Return", "10 Yr Rolling Return", "15 Yr Rolling Return", "20 Yr Rolling Return",
-               "Drawdown"]
+               "Drawdown",
+               "Since Inception Average Win Per Day", "YTD Average Win Per Day", "1 Yr Average Win Per Day",
+               "3 Yr Average Win Per Day", "5 Yr Average Win Per Day"
+        ]
 
         df = pd.DataFrame(data_list, columns=col)
         df.fillna(0)
