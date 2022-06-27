731b7187374c117b72888b303e4f73b9284dd346
jwshc <jwshc@connect.hku.hk>
2022-06-27 10:08:14 +0800

    update API

--- engine/api_engine/api_try_engine.py
+++ engine/api_engine/api_try_engine.py
@@ -1,43 +1,58 @@
-from IndexTradingApplication.engine.mongoDB_engine.mongodb_engine import mongodb_engine
+import certifi
+from pymongo import MongoClient
 from flask import Flask, app
 from flask_restful import Api, Resource
 
-# @app.route('/mainpage')
-def all_algo_1a():
-    mongo = mongodb_engine("simulation")
-    col = mongo.db.backtest_portfolio_rebalance_0
-    data = col.find({}, {"_id": 0, "NetLiquidation": 1, "timestamp": 1}).sort("_id", 1)
-    for x in data: print(x)
-    return data
+class Api_Mongodb:
+    def __init__(self,database):
+        try:
+            self.conn = MongoClient('mongodb+srv://Garylam:Lamindexinvest123!@mathtrade.yvcna.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=certifi.where())
+            self.db = self.conn[database]
+            print(f"Successful connection to database: {self.db.name}")
+        except:
+            print("WARNING: Could not connect to MongoDB")
 
-def all_algo_1b():
-    mongo = mongodb_engine()
-    col = mongo.db.Strategies
-    data1 = col.find({}, {"_id": 0, "strategy_name": 1, "YTD Sharpe": 1, "5 Yr Sharpe": 1, "YTD Return": 1, "5 Yr Return": 1, "Margin Ratio": 1}).limit(2).sort("YTD Return", -1)
-    data2 = col.find({}, {"_id": 0, "strategy_name": 1, "YTD Sharpe": 1, "5 Yr Sharpe": 1, "YTD Return": 1, "5 Yr Return": 1, "Margin Ratio": 1}).limit(2).sort("YTD Return", 1)
-    temp1 = []
-    for x in data1:
-        print(x)
-        temp1.append(x)
-    temp2 = []
-    for x in data2:
-        print(x)
-        temp2.append(x)
-    final = {}
-    final["top"] = temp1
-    final ["bottom"] = temp2
-    print(final)
-    # for x in data2: print(x)
-    # print({"top": data1, "bottom": data2})
-    return {data1, data2}
+    # @app.route('/mainpage')
+    def all_algo_1a(self):
+        mongo = Api_Mongodb("simulation")
+        col = mongo.db.backtest_portfolio_rebalance_0
+        data = col.find({}, {"_id": 0, "NetLiquidation": 1, "timestamp": 1}).sort("_id", 1)
+        for x in data: print(x)
+        return data
 
-def convert_df_to_json(self, df):
-    json_file = df.to_json(orient='records')
-    return json_file
+    def all_algo_1b(self):
+        mongo = Api_Mongodb()
+        col = mongo.db.Strategies
+        data1 = col.find({}, {"_id": 0, "strategy_name": 1, "YTD Sharpe": 1, "5 Yr Sharpe": 1, "YTD Return": 1, "5 Yr Return": 1, "Margin Ratio": 1}).limit(2).sort("YTD Return", -1)
+        data2 = col.find({}, {"_id": 0, "strategy_name": 1, "YTD Sharpe": 1, "5 Yr Sharpe": 1, "YTD Return": 1, "5 Yr Return": 1, "Margin Ratio": 1}).limit(2).sort("YTD Return", 1)
+        temp1 = []
+        for x in data1:
+            print(x)
+            temp1.append(x)
+        temp2 = []
+        for x in data2:
+            print(x)
+            temp2.append(x)
+        final = {}
+        final["top"] = temp1
+        final ["bottom"] = temp2
+        print(final)
+        # for x in data2: print(x)
+        # print({"top": data1, "bottom": data2})
+        return {data1, data2}
+
+    def all_algo_1c(self):
+        # mongo =
+        return
+
+    def convert_df_to_json(self, df):
+        json_file = df.to_json(orient='records')
+        return json_file
 
 def main():
+    pass
     # all_algo_1a()
-    all_algo_1b()
+    # all_algo_1b()
 
 if __name__ == "__main__":
-    main()
+        main()
