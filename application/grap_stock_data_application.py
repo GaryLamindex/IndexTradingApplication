41c814f3007b9d21d3bfc184b1155d7ac99e79d9
ss8-local-server <96756548+GaryLamindex@users.noreply.github.com>
2022-06-28 12:06:33 +0800

    a

--- application/grap_stock_data_application.py
+++ application/grap_stock_data_application.py
@@ -12,9 +12,9 @@ sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.resolve()))
 def main():
     # if you want to change the output path, go to __init__ of ibkr_stock_data_io_engine to change it
     # now the default output path is dynamodb/ticker_data/one_min
-    # ib = IB()
-    # ib.connect('127.0.0.1', 7497, clientId=1)
-    engine = ibkr_stock_data_io_engine()
+    ib = IB()
+    ib.connect('127.0.0.1', 7497, clientId=1)
+    engine = ibkr_stock_data_io_engine(ib)
 
     # The following statement won't work all the time
     # if the process is interrupted in the middle,
