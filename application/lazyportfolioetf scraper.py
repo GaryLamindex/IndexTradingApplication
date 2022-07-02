2475861eea06bdc111ae318172ab9c871355a8c5
Percy124 <percychui11@gmail.com>
2022-07-02 16:16:50 +0800

    update

--- application/lazyportfolioetf scraper.py
+++ application/lazyportfolioetf scraper.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
 import pandas as pd
+import requests
 
 
 def main():
@@ -10,33 +10,51 @@ def main():
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
     soup = BeautifulSoup(html, "lxml")
-    find = False
-    a = list()
+    portfolio_link = False
+    links = list()
     for link in soup.find_all('a', href=True):
         if link['href'] == "http://www.lazyportfolioetf.com/allocation/10-year-treasury/":
-            find = True
+            portfolio_link = True
         if link[
             'href'] == "https://twitter.com/intent/tweet?text=Lazy%20Portfolios:%20ETF%20Allocation&url=http://www.lazyportfolioetf.com/allocation":
-            find = False
-        if find:
-            a.append(link['href'])
-    a = list(dict.fromkeys(a))
-    weight = list()
-    ticker_name = list()
-    for x in range(0, len(a)):
-        tmp = re.findall(r'<b>(.*?)</b></a></td>', urlopen(a[x]).read().decode("utf-8"))
-        ticker_name.append(tmp)
-        tmp1 = (re.findall(r'<td class="w3-center" style=""><b>(.*?)</b></td>', urlopen(a[x]).read().decode("utf-8")))
-        if len(tmp) != len(tmp1):
-            tmp1 = tmp1[:-2]
-        weight.append(tmp1.copy())
-    weight = [x for xs in weight for x in xs]
-    ticker_name = [x for xs in ticker_name for x in xs]
-    df = pd.DataFrame()
-    df["weight"] = weight
-    df["ticker"] = ticker_name
+            portfolio_link = False
+        if portfolio_link:
+            links.append(link['href'])
+    links = list(dict.fromkeys(links))
+    df = pd.DataFrame(columns=['Weight', 'Ticker'])
+    for x in range(0, len(links)):
+        soup1 = BeautifulSoup(requests.get(links[x]).text, "lxml")
+        table = soup1.find('table',
+                           class_='w3-table table-padding-small w3-small font-family-arial table-valign-middle')
+        for row in table.tbody.find_all('tr'):
+            columns = row.find_all('td')
+            if columns != []:
+                weight = columns[0].b.contents[0]
+                ticker = columns[2].b.contents[0]
+                df = pd.concat([df, pd.DataFrame.from_records([{'Weight': weight, 'Ticker': ticker}])])
     df.to_excel("/Users/percychui/Downloads/scraper.xlsx", index=False)
-
+    """
+    dataframe with only two columns, but there will exist same ticker with different weights as there are many 
+    investment themes
+    """
+    df2 = pd.DataFrame(columns=['Weight', 'Ticker', 'ETF_name', 'Investment theme'])
+    for x in range(0, len(links)):
+        soup2 = BeautifulSoup(requests.get(links[x]).text, "lxml")
+        table = soup2.find('table',
+                           class_='w3-table table-padding-small w3-small font-family-arial table-valign-middle')
+        for row in table.tbody.find_all('tr'):
+            columns = row.find_all('td')
+            if columns != []:
+                weight = columns[0].b.contents[0]
+                ticker = columns[2].b.contents[0]
+                etf_name = columns[3].a.contents[0]
+                invest = columns[4].text.strip()
+                df2 = pd.concat([df2, pd.DataFrame.from_records(
+                    [{'Weight': weight, 'Ticker': ticker, 'ETF_name': etf_name, 'Investment theme': invest}])])
+    df2.to_excel("/Users/percychui/Downloads/scraper2.xlsx", index=False)
+    """
+    dataframe with whole table
+    """
 
 if __name__ == "__main__":
     main()
