ff5b08228afe7b84e9bf79a58afad880903f4186
GaryLamindex <garylam@lamindexinvest.com>
2022-07-23 16:13:50 +0800

    a

--- algo/rebalance_margin_wif_max_drawdown_control/web3_algo.py
+++ algo/rebalance_margin_wif_max_drawdown_control/web3_algo.py
@@ -2,6 +2,8 @@ import json
 
 from web3 import Web3, HTTPProvider
 
+BIGNUMBER = 1000000000000000000000000
+
 class web3_algo:
     def __init__(self, contract_address, contract_abi, account_address):
         self.contract_address = contract_address
@@ -14,18 +16,44 @@ class web3_algo:
 
 
     def inputRealTimeTickerData(self, ticker, price, last, timestamp):
+        price = int(price * BIGNUMBER)
+        last = int(last * BIGNUMBER)
         self.contract.functions.inputRealTimeTickerData(ticker, price, last, timestamp).transact()
 
     def inputMarginAccount(self, timestamp, fullInitMarginReq, fullMainMarginReq):
+        fullInitMarginReq = int(fullInitMarginReq * BIGNUMBER)
+        fullMainMarginReq = int(fullMainMarginReq * BIGNUMBER)
         self.contract.functions.inputMarginAccount(timestamp, fullInitMarginReq, fullMainMarginReq).transact()
 
+
     def inputTradingFunds(self, timestamp, availableFunds, excessLiquidity, buyingPower, leverage, equityWithLoanValue):
+        availableFunds = int(availableFunds * BIGNUMBER)
+        excessLiquidity = int(excessLiquidity * BIGNUMBER)
+        buyingPower = int(buyingPower * BIGNUMBER)
+        leverage = int(leverage * BIGNUMBER)
+        equityWithLoanValue = int(equityWithLoanValue * BIGNUMBER)
+
         self.contract.functions.inputTradingFunds(timestamp, availableFunds, excessLiquidity, buyingPower, leverage, equityWithLoanValue).transact()
 
     def inputMktValue(self, timestamp, totalCashValue, netDividend, netLiquidation, unrealizedPnL, realizedPnL, grossPositionValue):
+        totalCashValue = int(totalCashValue * BIGNUMBER)
+        netDividend = int(netDividend * BIGNUMBER)
+        netLiquidation = int(netLiquidation * BIGNUMBER)
+        unrealizedPnL = int(unrealizedPnL * BIGNUMBER)
+        realizedPnL = int(realizedPnL * BIGNUMBER)
+        grossPositionValue = int(grossPositionValue * BIGNUMBER)
         self.contract.functions.inputMktValue(timestamp, totalCashValue, netDividend, netLiquidation, unrealizedPnL, realizedPnL, grossPositionValue).transact()
 
     def updatePortfolioHoldingsData(self, timestamp, tickerName, position, marketPrice, averageCost, marketValue, realizedPNL, unrealizedPNL, initMarginReq, maintMarginReq, costBasis):
+        position = int(position * BIGNUMBER)
+        marketPrice = int(marketPrice * BIGNUMBER)
+        averageCost = int(averageCost * BIGNUMBER)
+        marketValue = int(marketValue * BIGNUMBER)
+        realizedPNL = int(realizedPNL * BIGNUMBER)
+        unrealizedPNL = int(unrealizedPNL * BIGNUMBER)
+        initMarginReq = int(initMarginReq * BIGNUMBER)
+        maintMarginReq = int(maintMarginReq * BIGNUMBER)
+        costBasis = int(costBasis * BIGNUMBER)
         self.contract.functions.updatePortfolioHoldingsData(timestamp, tickerName, position, marketPrice, averageCost, marketValue, realizedPNL, unrealizedPNL, initMarginReq, maintMarginReq, costBasis).transact()
 
     def getTickerActionMsg(self, ticker, timestamp):
@@ -74,7 +102,10 @@ class web3_algo:
         self.contract.functions.reinitializeContractState().call()
 
     def inputLoopWithTimestamps(self, loop, timestamp):
-        self.contract.functions.inputLoopWithTimestamps(loop, timestamp).call()
+        self.contract.functions.inputLoopWithTimestamps(loop, timestamp).transact()
+
+    def run(self, timestamp):
+        self.contract.functions.run(timestamp).call()
 
 def contract_test(algo):
 
@@ -112,12 +143,22 @@ def contract_run_test(algo):
     algo.reinitializeContractState()
     timestamp = 1564656465
 
-    algo.inputRealTimeTickerData("AAPL", 100, 100, timestamp)
-    algo.inputRealTimeTickerData("GOOG", 100, 100, timestamp)
+    algo.inputRealTimeTickerData("QQQ", 65.11, 65.11, timestamp)
 
-    algo.inputMarginAccount(timestamp, 100, 100)
-    algo.inputTradingFunds(timestamp, 100, 100, 100, 100, 100)
-    algo.inputMktValue(timestamp, 100, 100, 100, 100, 100, 100)
+    algo.inputMarginAccount(timestamp, 0, 0)
+    algo.inputTradingFunds(timestamp, 1000000, 1000000, 6666666, 0, 1000000)
+    algo.inputMktValue(timestamp, 1000000, 0, 1000000, 0, 0, 0)
+
+    print(algo.getTickerActionMsg("QQQ", timestamp))
+    print(algo.getPortfolioHolding("QQQ", timestamp))
+    print(algo.getRealTimeTickerData("QQQ", timestamp))
+    print(algo.getMarginAccount(timestamp))
+    print(algo.getTradingFunds(timestamp))
+    print(algo.getMktValue(timestamp))
+
+    algo.run(timestamp)
+    print(algo.getTickerActionMsg("QQQ", timestamp))
+
 
 
 if __name__ == '__main__':
@@ -125,10 +166,10 @@ if __name__ == '__main__':
         info_json = json.load(f)
     abi = info_json["abi"]
 
-    address = "0x383B32AeC5074c740815aC8d15EfABaE4518E90D"
+    address = "0x7F323672c21223B94D5C24C12b07c08fcdCc6b1D"
     default_account = "0xE357eaFcE4d472c344e638E72ce55dE8C6b62992"
     algo = web3_algo(address, abi, default_account)
-    contract_test(algo)
+    contract_run_test(algo)
 
 
 
