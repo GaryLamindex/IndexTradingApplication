9902b57df2d64a2ef7d99a67e5c89f34aaaa04d4
Thisiss0710 <82769462+Thisiss0710@users.noreply.github.com>
2022-08-01 17:26:44 +0800

    Update grab_data_application.py

--- application/grab_data_application.py
+++ application/grab_data_application.py
@@ -9,9 +9,9 @@ def main():
     # Download missing data
     # Run indefinitely if True
     infinite_run = True
-    # ib = IB()
-    # ib.connect('127.0.0.1', 7497, clientId=1)
-    stock_engine = grab_stock_data_engine()
+    ib = IB()
+    ib.connect('127.0.0.1', 7497, clientId=1)
+    stock_engine = grab_stock_data_engine(ib_instance=ib)
     crypto_engine = grab_crypto_data_engine()
 
     last_exec_timestamp = None
@@ -22,11 +22,13 @@ def main():
     last_exec_timestamp = dt.datetime.now()
     # Download data indefinitely
     while infinite_run:
+        # Download daily stock data
         now_timestamp = dt.datetime.now()
-        if (now_timestamp - last_exec_timestamp).total_seconds() >= 43200:
-            stock_engine.get_missing_daily_data()
+        if (now_timestamp - last_exec_timestamp).total_seconds() >= 86400:
             last_exec_timestamp = dt.datetime.now()
-        time.sleep(3600)
+            stock_engine.get_missing_daily_data()
+        # Download per minute stock data
+        stock_engine.get_multiple_min_data_by_range(0, dt.datetime.now().timestamp(), '1 min', False, tickers=None)
 
 
 if __name__ == "__main__":
